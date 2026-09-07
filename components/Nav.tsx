1afbfc2b80dadbb52f7e90e7e1c2330bf11fbeda
GeromeFehr <geromefehr@gmail.com>
2026-09-07 18:59:45 +0200

    Make components/Nav.tsx universe-aware

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
-export default function Nav(){
+import { currentUser } from "@/lib/auth";
+
+export default async function Nav(){
+  const user=await currentUser();
   return <header className="topbar">
     <Link className="brand" href="/"><span className="brandDot"/>CAREER//UNIVERSE</Link>
     <nav className="navlinks">
-      <Link href="/">Feed</Link><Link href="/schedule">Spielplan</Link><Link href="/career">Karriere</Link>
+      {user?<><Link href="/">Feed</Link><Link href="/schedule">Spielplan</Link><Link href="/career">Karriere</Link>
       <Link href="/media">News</Link><Link href="/social">Social</Link><Link href="/trades">Trades</Link>
-      <Link href="/awards">Awards</Link><Link href="/admin">Control Room</Link>
+      <Link href="/awards">Awards</Link><Link href="/admin">Control Room</Link><Link href="/universes">Universen</Link></>
+      :<><Link href="/login">Login</Link><Link href="/register">Registrieren</Link></>}
     </nav>
   </header>
 }
